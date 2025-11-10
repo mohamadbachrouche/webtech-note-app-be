7ee917dd5c2acaf05c947feb4e86046d191b8a93
mohamadbachrouche <mohamadbachrouche@outlook.com>
2025-11-10 19:10:21 +0100

    add methods to manage notes in trash; implement retrieval and restoration functionality

--- src/main/java/de/htw/webtech/repository/NoteRepository.java
+++ src/main/java/de/htw/webtech/repository/NoteRepository.java
@@ -5,4 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NoteRepository extends CrudRepository<Note, Long> {}
+public interface NoteRepository extends CrudRepository<Note, Long> {
+    // --- ADD THESE TWO LINES ---
+    Iterable<Note> findAllByInTrashFalse();
+    Iterable<Note> findAllByInTrashTrue();
+}
