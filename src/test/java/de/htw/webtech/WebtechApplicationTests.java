9567122c84b06fd5640c05b265acae829fd2ce89
mohamadbachrouche <mohamadbachrouche@outlook.com>
2025-12-22 16:23:15 +0100

    remove unused test files and add detailed Gradle test reports

--- src/test/java/de/htw/webtech/WebtechApplicationTests.java
+++ src/test/java/de/htw/webtech/WebtechApplicationTests.java
@@ -3,7 +3,12 @@ package de.htw.webtech;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// We manually overwrite the database settings just for this test
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb",
+		"spring.datasource.driverClassName=org.h2.Driver",
+		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 class WebtechApplicationTests {
 
 	@Test
