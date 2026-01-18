cd58cc31f25ade45850d2b9c315d96cbbe30d3a0
mohamadbachrouche <mohamadbachrouche@outlook.com>
2026-01-18 16:01:40 +0100

    Add centralized H2 test database configuration

--- src/test/java/de/htw/webtech/WebtechApplicationTests.java
+++ src/test/java/de/htw/webtech/WebtechApplicationTests.java
@@ -3,12 +3,8 @@ package de.htw.webtech;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// We manually overwrite the database settings just for this test
-@SpringBootTest(properties = {
-		"spring.datasource.url=jdbc:h2:mem:testdb",
-		"spring.datasource.driverClassName=org.h2.Driver",
-		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
-})
+// Test configuration is centralized in src/test/resources/application.properties
+@SpringBootTest
 class WebtechApplicationTests {
 
 	@Test
